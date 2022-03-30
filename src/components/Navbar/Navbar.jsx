f09a50ca79b4138f3625d2654a5c345f13dfdd8c
Jorge196 <JoRamirez96@gmail.com>
2022-03-30 15:25:51 -0400

    change the jsx file for Navbar inorder to had the cakeslice drop menu when the app isnot on full screen, also made the css file adjustments in order for the app to look well and fit mobile devices. Also changed the main colors of the app, still deciding what colors to actually change it to

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import { GiCakeMenu } from 'react-icons/gi';
-import { MdOutlineCakeMenu } from 'react-icons/md';
+import { GiCakeSlice } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
 import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => (
-  <nav className="app__navbar">
-    <div className="app__navbar-logo">
-      <img src={images.blackrubberspatula}  alt="app logo" />
-    </div>
-    <ul className="app__navbar-links">
-      <li className="p__opensans"><a href="#home">Home</a></li>
-      <li className="p__opensans"><a href="#about">About</a></li>
-      <li className="p__opensans"><a href="#cake">Cakes</a></li>
-      <li className="p__opensans"><a href="#contact">Contact</a></li>
-
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+  return(
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.blackrubberspatula}  alt="app logo" />
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans"><a href="#home">Home</a></li>
+        <li className="p__opensans"><a href="#about">About</a></li>
+        <li className="p__opensans"><a href="#cake">Cakes</a></li>
+        <li className="p__opensans"><a href="#contact">Contact</a></li>
+      </ul>
+      <div className="app__navbar-smallscreen">
+        <GiCakeSlice color="#fff" fontsize={27} onClick={() => setToggleMenu(true)} />
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onCLick={() => setToggleMenu(false)} />      
+            <ul className="app__navbar-smallscreen_links">
+              <li className="p__opensans"><a href="#home">Home</a></li>
+              <li className="p__opensans"><a href="#about">About</a></li>
+              <li className="p__opensans"><a href="#cake">Cakes</a></li>
+              <li className="p__opensans"><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+        )}
+      </div>  
+    </nav>
+  );
+};
 
 export default Navbar;
